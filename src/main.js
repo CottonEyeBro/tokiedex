2b9186264b519333c85c90909e3af69d73e20175
Cooper <55swimmer55@gmail.com>
2025-02-20 00:19:03 -0500

    remove strictMode from main.js

--- src/main.js
+++ src/main.js
@@ -9,11 +9,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </WagmiProvider>
-  </StrictMode>
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </WagmiProvider>
 );
