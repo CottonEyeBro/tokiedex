475feee751e65159665be73466facd526ab132b6
Cooper <55swimmer55@gmail.com>
2025-02-20 00:24:41 -0500

    revert to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </WagmiProvider>
+  <StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>
 );
