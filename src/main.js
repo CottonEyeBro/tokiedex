46c2896db46df150fba6f0a1c891c8e3979d1fb1
Cooper <55swimmer55@gmail.com>
2025-02-20 00:15:07 -0500

    change to main.js from main.jsx and update relevant files to match

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { WagmiProvider } from 'wagmi';
-import { config } from './wagmi';
+import { config } from './wagmi.js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
